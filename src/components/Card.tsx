0dedb164d6ee0dc16cbcd7d7f12189d3aebae93f
leonardoRocchini <leonardo.rocchini.00@gmail.com>
2023-12-07 18:49:49 +0100

    added
    added

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+
+interface Props {
+	title: string;
+	body: string;
+	href: string;
+}
+
+export const Card = () => {
+	
+//const { href, title, body } = props
+
+return (
+	<div>test card</div>
+)
+
+
+}
